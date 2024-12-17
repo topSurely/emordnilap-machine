75c5e28a592b0f3a8ab2e74abeb1eab1f70b0381
Jason Geiger <jason.phil.geiger@gmail.com>
2024-12-16 17:54:13 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,14 @@ function App() {
           justifyContent: "center"
         }}>
 
-          <input value={textValue} style={{ position: "absolute", fontSize: "2rem" }}
+          <MachineStage text={textValue} height={500} width={1000} />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+
+          <input value={textValue} style={{ fontSize: "2rem" }}
             onChange={(e) => {
               setTextValue(e.target.value.replace(/[^a-zA-Z]/g, "").toUpperCase())
             }} />
-          <MachineStage text={textValue} height={500} width={1000} />
         </div>
         <div style={{ textAlign: "center" }}>
           {Palindromes.map((v) => {
